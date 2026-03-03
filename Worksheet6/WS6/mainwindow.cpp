d5d9ed41a1e229d57c2209a064f9fa03338686fc
Melody Joseph <evymj3@nottingham.ac.uk>
2026-03-03 14:35:51 +0000

    Finished Exercise 4: TreeView now displays parts

--- Worksheet6/WS6/mainwindow.cpp
+++ Worksheet6/WS6/mainwindow.cpp
@@ -1,4 +1,4 @@
-#include <QMessageBox> // Added from PDF
+#include <QMessageBox>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -8,37 +8,49 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // Example of connecting signals and slots in mainwindow.cpp
-    // =====================================================================
-    // Add the following line at the end of the MainWindow constructor
-
+    // Existing connections for button and status bar
     connect(ui->pushButton, &QPushButton::released, this, &MainWindow::handleButton);
-    // =====================================================================
-
-    // Example of connecting to StatusBar signal mainwindow.cpp=============
-    // This needs adding to end of MainWindow constructor
-
-    // Connect the statusUpdateMessage() signal to the showMessage() slot of the status bar
     connect(this, &MainWindow::statusUpdateMessage, ui->statusbar, &QStatusBar::showMessage);
-    // =====================================================================
-}
 
+    /* 1. Create/allocate the ModelList */
+    this->partList = new ModelPartList("PartsList");
 
+    /* 2. Link it to the treeview in the GUI */
+    ui->treeView->setModel(this->partList);
+
+    /* 3. Manually create a model tree (Exercise 4) */
+    ModelPart *rootItem = this->partList->getRootItem();
+
+    /* Add 3 top level items */
+    for (int i = 0; i < 3; i++) {
+        /* Create strings for both data columns */
+        QString name = QString("TopLevel %1").arg(i);
+        QString visible("true");
+
+        /* Create child item */
+        ModelPart *childItem = new ModelPart({ name, visible });
+
+        /* Append to tree top-level */
+        rootItem->appendChild(childItem);
+
+        /* Add 5 sub-items */
+        for (int j = 0; j < 5; j++) {
+            QString subName = QString("Item %1,%2").arg(i).arg(j);
+            QString subVisible("true");
+
+            ModelPart *childChildItem = new ModelPart({ subName, subVisible });
+
+            /* Append to parent */
+            childItem->appendChild(childChildItem);
+        }
+    }
+} // <--- End of Constructor
 
 MainWindow::~MainWindow()
 {
     delete ui;
-
 }
 
-
-
-//replaced with new
-// Update your button handling slot so it activates status bar
-// =====================================================================
 void MainWindow::handleButton() {
-    // This causes MainWindow to emit the signal that will then be
-    // received by the status bar’s slot
     emit statusUpdateMessage(QString("Add button was clicked"), 0);
 }
-// =====================================================================
