6088ccfe5ef75a670bed58c08d451a059eb52358
Melody Joseph <evymj3@nottingham.ac.uk>
2026-04-18 20:06:30 +0100

    Worksheet 5: Updated adder.h to WS3 Exercise 5 DLL version with full Doxygen comments

--- Worksheet5/adder.h
+++ Worksheet5/adder.h
@@ -1,13 +1,30 @@
-#ifndef ADDER_H
-#define ADDER_H
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
 
-// The PDF mentions a definition for static/shared; this ensures it works
-#ifdef maths_STATIC
-#define MATHSLIB_API
+/** @file
+ * This file contains the declarations of all exported functions in the maths library.
+ */
+
+#if defined(_WIN32) || defined(WIN32)
+  #ifdef maths_STATIC
+    #define MATHSLIB_API
+  #else
+    #ifdef maths_EXPORTS
+      #define MATHSLIB_API __declspec(dllexport)
+    #else
+      #define MATHSLIB_API __declspec(dllimport)
+    #endif
+  #endif
 #else
-#define MATHSLIB_API
+  #define MATHSLIB_API
 #endif
 
+/** Adding function [Brief description]
+ * This function adds two numbers [More detail]
+ * @param a is the first number [Parameter definition]
+ * @param b is the second number [Parameter definition]
+ * @return sum of a and b [Return value description]
+ */
 MATHSLIB_API int add(int a, int b);
 
 #endif
